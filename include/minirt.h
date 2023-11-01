4aa333082b2accbf293a2c04fdfb34cf275e3812
Mehdi Mirzaie <mehdimirzaie18@gmail.com>
2023-11-02 10:04:10 +1030

    fix segfaults, due to invalid initialization

--- include/minirt.h
+++ include/minirt.h
@@ -6,6 +6,8 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include "map.h"
+#include <stdio.h>
+# include "map.h"
 
 // Srceen dimensions
 # define SIZE 700
@@ -25,6 +27,27 @@
 # define SCROLL_UP 4
 # define SCROLL_DOWN 5
 
+typedef struct
+{
+    float x, y, z;
+} vec3d;
+
+typedef struct
+{
+    vec3d p[3];
+} triangle;
+
+typedef struct
+{
+    triangle* tris;
+	int num_triangles;
+} mesh;
+
+typedef struct
+{
+    float m[4][4];
+} mat4x4;
+
 typedef struct s_rt
 {
 	t_map	*map;
@@ -42,10 +65,27 @@ typedef struct s_rt
 	double	offset_y;
 	double	zoom;
 	int		color;
+	float	fTheta;
+	mat4x4	*matProj;
+	mesh 	meshCube;
 }			t_rt;
 
 // src/init.c
 void	init_rt(t_rt *rt);
 void	init_mlx(t_rt *rt);
+
+void	clearScreen(t_rt *rt);
+
+// int	key_hook(int keycode, t_rt *rt);
+int main(void);
+
+
+triangle	*init_cube(void);
+mat4x4	*init_matProj(void);
+
+// void draw(t_rt *rt, mat4x4 *matProj, mesh *meshCube);
+int draw(t_rt *rt);
+
+int	key_hook(int keycode, t_rt *rt);
 
 #endif
